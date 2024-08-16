b4755255fa8a55dd8d3db896cfae81ae71165e47
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-08-16 14:14:25 +0900

    Create fcLayer.h

---
+++ ResNet-18/fcLayer.h
@@ -0,0 +1,20 @@
+﻿#ifndef FCLAYER_H
+#define FCLAYER_H
+#include <string>
+
+class FcLayer
+{
+public:
+    FcLayer(int nInputSize, int nOutputSize);
+    ~FcLayer();
+    void forward(float *pfInput);
+    float *GetOutput();
+    int GetOutputSize();
+    void ReadFcWb();
+    
+private:
+    int m_nInputSize, m_nOutputSize, m_nWeightSize, m_nRelubool;
+    float *m_pfWeight, *m_pfBias, *m_pfOutput;
+};
+
+#endif
