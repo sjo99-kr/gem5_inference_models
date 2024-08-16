a1d8a20b0c12de8ed824e2afd29cb7ace7f5bb5b
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-08-16 14:15:24 +0900

    Create averagepoolLayer.h

---
+++ ResNet-18/averagepoolLayer.h
@@ -0,0 +1,19 @@
+﻿#ifndef POOLAYER_H
+#define POOLAYER_H
+
+class AveragePoolLayer
+{
+public:
+    AveragePoolLayer(int nOutputNum, int nInputWidth);
+    ~AveragePoolLayer();
+    void forward(float *pfInput);
+    float *GetOutput();
+
+private:
+    int m_nOutputNum, m_nPoolWidth, m_nInputWidth;
+    int m_nPoolSize, m_nOutputWidth, m_nInputSize, m_nOutputSize;
+    
+    float *m_pfOutput;
+};
+
+#endif
