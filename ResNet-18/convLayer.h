a7d1cdbf852f40e768080a15579138794523f4a1
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-08-16 14:19:36 +0900

    Create convLayer.h

---
+++ ResNet-18/convLayer.h
@@ -0,0 +1,23 @@
+﻿#ifndef CONVLAYER_H
+#define CONVLAYER_H
+#include <string>
+
+class ConvLayer
+{
+public:
+    ConvLayer(int nInputNum, int nOutputNum, int nInputWidth, int nKernelWidth, int nPad, int nStride, int group);
+
+    void forward(float *pfInput);
+    float *GetOutput();
+    void Addpad(float *pfInput);
+    int GetOutputSize();
+
+private:
+    int m_nInputNum, m_nOutputNum, m_nInputWidth, m_nKernelWidth, m_nPad, m_nStride, m_nGroup, m_nInputGroupNum, m_nOutputGroupNum;
+    float *m_pfInputPad, *m_pfOutput;
+    float *m_pfWeight;
+    float *m_pfBias;
+    int m_nInputPadWidth, m_nOutputWidth;
+    int m_nKernelSize, m_nInputSize, m_nInputPadSize, m_nOutputSize;
+};
+#endif
