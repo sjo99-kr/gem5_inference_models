3bdbd61399940ae41082c14e43dca856fbc5bd3c
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-08-16 13:46:42 +0900

    Create averagepoolLayer.cpp

---
+++ MobileNetV2/averagepoolLayer.cpp
@@ -0,0 +1,44 @@
+﻿#include "averagepoolLayer.h"
+#include <iostream>
+
+
+AveragePoolLayer::AveragePoolLayer(int nOutputNum, int nInputWidth):
+                        m_nOutputNum(nOutputNum), m_nInputWidth(nInputWidth), m_nPoolWidth(nInputWidth)
+{
+    m_nOutputWidth = 1; //Average pool (1x1) 
+    m_nOutputSize = m_nOutputWidth *m_nOutputWidth;
+    m_nInputSize = m_nInputWidth * m_nInputWidth;
+    
+    m_pfOutput = new float [m_nOutputNum * m_nOutputSize];
+}
+
+AveragePoolLayer::~AveragePoolLayer()
+{
+    delete[] m_pfOutput;
+}
+
+void AveragePoolLayer::forward(float *pfInput)
+{
+    for (int nOutmapIndex = 0; nOutmapIndex < m_nOutputNum; nOutmapIndex++)
+    {
+        int nInputIndexStart, nInputIndex, nOutputIndex;
+        nOutputIndex = nOutmapIndex * m_nOutputSize;
+        nInputIndexStart = nOutmapIndex * m_nInputSize;
+        float fSum = 0;
+        for (int m = 0; m < m_nPoolWidth; m++)
+        {
+            for (int n = 0; n<m_nPoolWidth; n++)
+            {
+                nInputIndex = nInputIndexStart + m * m_nInputWidth + n;
+                fSum += pfInput[nInputIndex];
+            }
+        }
+
+        m_pfOutput[nOutputIndex] = fSum / m_nInputSize;
+    }    
+}
+
+float *AveragePoolLayer::GetOutput()
+{
+    return m_pfOutput;
+}
